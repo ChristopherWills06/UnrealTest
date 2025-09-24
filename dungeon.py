0bd1cc7c25aa868b9be465ba46a491a1febdc752
ChristopherWills06 <scarlett.williams090@escapestudios.ac.uk>
2025-09-24 20:13:33 +0100

    Added heo & character, tweaked dungeon

--- dungeon.py
+++ dungeon.py
@@ -1,4 +1,4 @@
-import random
+
 
 hero_stats = {
     "name" : "hero", # key : value (name -> key) (hero -> value)
@@ -10,6 +10,10 @@ enemy_stats = {
     "health" : 20,
  }
 
+hero_max_health = 100
+
+health_potion_strength = 5
+hero_inventory = ["sword", "health potion", "rope"]
 
 
 def quit ():
@@ -22,49 +26,70 @@ def player_stats ():
     for key, value in hero_stats.items():
         print(f"{key} : {value}" )
 
-def goblin_stats ():
-    print ("\nGoblin Health Stats:")
-    for key, value in enemy_stats.items():
-        print(f"{key} : {value}" )
-
 def player_move():
     pass
 
 def player_attack():
     pass
 
+def player_heal(item_name):
+
+    if (hero_inventory.count("health potion") <=0 ):
+        print (f"You don't have any items {item_name}")
+        return
+
+    print (f"You used a {item_name} you've restored {health_potion_strength} health")
+    hero_stats["health"] = hero_stats["health"] + health_potion_strength
+
+    # health = 100
+    # use health potion
+    # print -> you alreayd have max health
+    # DO NOT remove the health potion
+
+    if (hero_stats["health"] >= hero_max_health):
+        print ("You've reached max health")
+        hero_stats["health"] = hero_max_health
+
+    print (f"Your health is now {hero_stats['health']}")
+    hero_inventory.remove("health potion")
+    print(f"Your inventory is now {hero_inventory}")
+
+def use_item():
+    item_name = input (f"What item do you want to use? {hero_inventory}\n").lower()
+    print (f"The item you want to use is {item_name} ")
+    match item_name:
+        case "sword":
+            pass
+        case "health potion":
+            player_heal(item_name)
+
+        case "rope":
+            pass
+        case _:
+            print(f"{item_name} is not in your inventory")
+
+#TEMP FUNCT
+def damage_player():
+    hero_stats["health"] = hero_stats["health"] - 10
+    print (f"Your health is now {hero_stats['health']}")
+
 isPlaying = True
 
 hero_stats["name"] = input ("What is your name?\n")
 player_stats()
-goblin_stats()
-
-
-
-# Nested loops to simulate a grid-based game world (e.g., a 2D map)
-print("\nDisplaying game world (5x5 grid):")
-game_world = [['.' for i in range(5)] for j in range(5)]
-# Place the player in the center of the grid
-player_position = (2, 2)
-game_world[player_position[0]][player_position[1]] = 'P'
-# Print the game world
-for row in game_world:
-    for cell in row:
-        print(cell, end=' ')
-    print()
-
-
 
 while (isPlaying):
 
-    action = input ("\nSelect Action: Attack, Move & Flee\n").lower()
-
+    action = input ("\nSelect Action: Attack, Move, Use or Flee\n").lower()
     print (f"Player Action: {action}")
 
     if (action == "flee"):
         isPlaying = quit() # <-- isPlaying = False
     elif (action == "attack"):
-        player_attack()
+        #player_attack()
+        damage_player()
+    elif (action == "use"):
+        use_item()
     elif (action == "move"):
         player_move()
 
---
+++ hero.py
@@ -0,0 +1,81 @@
+
+#Character ____ Hero
+#         |____ Enemy
+
+
+# TODO
+# Player heal
+# Use items
+# Connect the two files
+
+
+
+class Hero ():
+    def __init__(self):
+
+        self.max_health = 100.0
+
+        self.health_potion_strength = 5
+
+        self.stats = {
+            "name" : "hero",
+            "strength" : 10,
+            "health" : 100.0,
+        }
+
+        self.inventory = ["sword", "leath potion", "rope"]
+
+    def print_stats(self):
+        print("Your stats are: ")
+        for key, value in self.stats.items():
+            print(f"{key} : {value}")
+
+    def set_name(self, name):
+        self.stats["name"] = name
+        self.print_stats
+
+    def move(self):
+        pass
+
+    def attack(self):
+        pass
+
+    def take_damage(self, damage):
+        self.stats["health"] = self.stats["health"] - damage
+        print(f"Your health is now {self.stats['health']}")
+        pass
+
+    def heal(self, item_name):
+        
+        #TODO > Move this to this use item function
+        if(self.inventory.count("health potion") <=0):
+            print(f"You don't have any {item_name}")
+
+        print (f"You used a {item_name} you've restored {self.health_potion_strength} health")
+        self.stats["health"] = self.stats["health"] + self.health_potion_strength
+
+        if (self.stats["health"]>= self.max_health):
+            print ("You've reached max health")
+            self.stats["health"] = self.max_health
+
+        print (f"Your health is now {self.stats['health']}")
+        self.inventory.remove("health potion")
+        print(f"Your inventory is now{self.inventory}")
+
+    def use_item(self):
+        pass
+
+#---------------------------
+
+player = Hero()
+
+# print(f"Here are your Hero Stats {player.stats}")
+
+hero = Hero()
+hero.print_stats()
+print("\n-------------------------------")
+hero.stats["health"] = 70
+hero.heal("health potion")
+print("\n-------------------------------")
+
+player.print_stats()
